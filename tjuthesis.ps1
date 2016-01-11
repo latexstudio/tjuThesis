42cfca40512602f59f5e298f9f2cf51857857b0a
svandex <svandex@gmail.com>
2016-01-11 16:20:09 +0800

    Powershell Script

---
+++ tjuthesis.ps1
@@ -0,0 +1,17 @@
+del *.dvi
+del *.out
+del *.log 
+del *.aux  
+del *.bbl    
+del *.blg  
+del *.brf  
+
+xelatex ./thesis.tex
+
+del *.dvi
+del *.out
+del *.log 
+del *.aux  
+del *.bbl    
+del *.blg  
+del *.brf  
